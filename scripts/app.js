ea12f403a0d48671cee5d939edd5bd9a90b69ad5
Spencer Mathews <spencer.mathews@gmail.com>
2017-07-22 16:29:49 -0700

    Add upload event listener

--- scripts/app.js
+++ scripts/app.js
@@ -132,10 +132,38 @@ if (navigator.getUserMedia) {
       let time = `${year}-${month}-${day}-${hour}-${min}-${sec}`;
 
       let storageRef = firebase.storage().ref("tmp").child(time + ".webm")
-      let uploadTask = storageRef.put(blob).then(function(snapshot) {
-        console.log("File uploaded:", snapshot.downloadURL, "("+snapshot.bytesTransferred, "bytes)");
+      let uploadTask = storageRef.put(blob);
+      // then is redundant when using the state_changed event listener
+      uploadTask.then(function(snapshot) {
+        console.log("File uploaded: ("+snapshot.totalBytes, "bytes)", snapshot.downloadURL,);
       });
-      console.log(uploadTask)
+      console.log(uploadTask);
+
+      // from https://firebase.google.com/docs/storage/web/upload-files, see also https://firebase.google.com/docs/reference/js/firebase.storage.UploadTask
+      // Register three observers:
+      // 1. 'state_changed' (firebase.storage.TaskEvent.STATE_CHANGED) observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on('state_changed', function(snapshot){
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function(error) {
+        // Handle unsuccessful uploads
+      }, function() {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        var downloadURL = uploadTask.snapshot.downloadURL;
+      });
 
       let data = {
         time: time
